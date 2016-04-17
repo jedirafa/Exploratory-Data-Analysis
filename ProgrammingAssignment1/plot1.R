5155923112715475eb525f2f0508f4e54be5723c
jedirafa <rafaelm.godinho@gmail.com>
2016-04-17 17:25:49 -0300

    first commit

---
+++ ProgrammingAssignment1/plot1.R
@@ -0,0 +1,48 @@
+##########################################################################################################
+
+## Coursera Exploratory Data Analysis Project 1 
+## Rafael Godinho
+## 2016-04-17
+
+# plot1.r File Description:
+
+# This script will perform the following steps on the UC Irvine Machine Learning Repository Dataset downloaded from 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip 
+# 1. The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate of how much memory the dataset will require in memory before reading into R. Make sure your computer has enough memory (most modern computers should be fine).
+# 2. We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data from just those dates rather than reading in the entire dataset and subsetting to those dates. 
+# 3. You may find it useful to convert the Date and Time variables to Date/Time classes in R using the strptime()  and as.Date() functions.
+# 4. Note that in this dataset missing values are coded as ?. 
+# 5. Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+# 6. Name each of the plot files as plot1.png, plot2.png, etc.
+# 7. Create a separate R code file (plot1.R, plot2.R, etc.) that constructs the corresponding plot, i.e. code in plot1.R constructs the plot1.png plot. Your code file should include code for reading the data so that the plot can be fully reproduced. You must also include the code that creates the PNG file
+# 8. Add the PNG file and R code file to the top-level folder of your git repository (no need for separate sub-folders)
+
+##########################################################################################################
+
+
+setwd("C:/Users/Rafael/Desktop/Data Science/project/4 - Exploratory Data Analysis/ProgrammingAssignment1")
+downloadURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+downloadFile <- "./Data/household_power_consumption.zip"
+householdFile <- "./Data/household_power_consumption.txt"
+
+##
+
+
+if (!file.exists(householdFile)) {
+        download.file(downloadURL, downloadFile, method = "curl")
+        unzip(downloadFile, overwrite = T, exdir = "./Data")
+}
+##
+
+plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+##
+## Generating Plot 1
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+## copy to png
+dev.copy(png, file="plot1.png")
+dev.off()
---
+++ README.md
@@ -0,0 +1,20 @@
+##########################################################################################################
+
+## Coursera Exploratory Data Analysis Project 1 
+## Rafael Godinho
+## 2016-04-17
+
+# File Description:
+
+# This script will perform the following steps on the UC Irvine Machine Learning Repository Dataset downloaded from 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip 
+# 1. The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate of how much memory the dataset will require in memory before reading into R. Make sure your computer has enough memory (most modern computers should be fine).
+# 2. We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data from just those dates rather than reading in the entire dataset and subsetting to those dates. 
+# 3. You may find it useful to convert the Date and Time variables to Date/Time classes in R using the strptime()  and as.Date() functions.
+# 4. Note that in this dataset missing values are coded as ?. 
+# 5. Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+# 6. Name each of the plot files as plot1.png, plot2.png, etc.
+# 7. Create a separate R code file (plot1.R, plot2.R, etc.) that constructs the corresponding plot, i.e. code in plot1.R constructs the plot1.png plot. Your code file should include code for reading the data so that the plot can be fully reproduced. You must also include the code that creates the PNG file
+# 8. Add the PNG file and R code file to the top-level folder of your git repository (no need for separate sub-folders)
+
+##########################################################################################################
